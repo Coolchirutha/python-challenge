7a3c66030a4002ac443fde6cda7c682b92275a0f
Mouni Nischal <d.chiranjeevi007@gmail.com>
2021-12-23 15:31:45 +0530

    Added the answer

--- 1.py
+++ 1.py
@@ -13,20 +13,26 @@ contents = contents.split("<font color=\"#f000f0\">")[-1]
 # Here we split the content till the end of the string and remove the newline characters at the start and end as well.
 contents = contents.split("</tr>")[0][2:-2]
 
-# Creating an empty string for the translated string
-res = ""
 
-# Iterate over the string and transpose every letter we find with
-# the second next. Example: a->c, b->d, ... , z->b
-for c in contents:
-  # Here 97 is the ASCII code for 'a' and 122 is for 'z'
-  if ord(c) >= 97 and ord(c) <=122:
-    # Checking for overflow
-    if ord(c) + 2 >= 123:
-      res += chr((ord(c) + 2) - 26)
+def transposition(s):
+  # Creating an empty string for the translated string
+  res = ""
+  # Iterate over the string and transpose every letter we find with
+  # the second next. Example: a->c, b->d, ... , z->b
+  for c in s:
+    # Here 97 is the ASCII code for 'a' and 122 is for 'z'
+    if ord(c) >= 97 and ord(c) <=122:
+      # Checking for overflow
+      if ord(c) + 2 >= 123:
+        res += chr((ord(c) + 2) - 26)
+      else:
+        res += chr(ord(c) + 2)
     else:
-      res += chr(ord(c) + 2)
-  else:
-    res += c
+      res += c
+  return res
 
-print(res)
+print(transposition(contents))
+
+# Solving for the answer
+toConv = url.split('/')[-1].split('.')[0]
+print(transposition(toConv))
